088627fc186fad8b12f600b6e0d11758c1c35dc9
AsaMalika25 <khairulasa9@gmail.com>
2024-06-12 15:59:45 +0700

    add my project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
   const menu = document.getElementById('menu');
   const navbar = document.querySelector('nav.navbar');
-  
-  menu.addEventListener('click', function() {
-    navbar.classList.toggle('nav-toggle');
-    menu.classList.toggle('fa-bars');
-    menu.classList.toggle('fa-times');
-  });
+
+  if (menu && navbar) {
+    menu.addEventListener('click', function() {
+      navbar.classList.toggle('nav-toggle');
+      menu.classList.toggle('fa-bars');
+      menu.classList.toggle('fa-times');
+    });
+  }
+
+  const slides = document.querySelectorAll('.img-slide');
+  const slider = document.querySelector('.slider');
+  let index = 0;
+
+  function showSlide(i) {
+    const totalSlides = slides.length;
+    const newTransformValue = -100 * i + '%';
+    slider.style.transform = `translateX(${newTransformValue})`;
+  }
+
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+  }
+
+  setInterval(nextSlide, 3000);
+
+  showSlide(index);
 });
