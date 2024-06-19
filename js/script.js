63e82f132ac94abeb0fbb80c7ede2db9fb49885c
AsaMalika25 <khairulasa9@gmail.com>
2024-06-19 16:46:44 +0700

    add blink text effect

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
+  window.addEventListener('load', function () {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('nav-toggle');
+
+    if (window.scrollY>60) {
+      document.querySelector('#scroll-top').classList.add('active');
+    }else{
+      this.document.querySelector('#scroll-top').classList.remove('active');
+    }
+    
+  })
+
+
   const slides = document.querySelectorAll('.img-slider, .img-slide');
   const slider = document.querySelector('.slider');
   let index = 0;
@@ -27,4 +40,31 @@ document.addEventListener('DOMContentLoaded', function() {
   setInterval(nextSlide, 3000);
 
   showSlide(index);
+
+
+  const dynamicText = document.getElementById('typing-text');
+  const words = ["Abdi Malika", "Web Developer", "Backend Developer"];
+  let wordIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChars = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChars;
+
+    if (!isDeleting && charIndex < currentWord.length) {
+      charIndex++;
+      setTimeout(typeEffect, 200);
+    } else if (isDeleting && charIndex > 0) {
+      charIndex--;
+      setTimeout(typeEffect, 100);
+    } else {
+      isDeleting = !isDeleting;
+      wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+      setTimeout(typeEffect, 1200);
+    }
+  }
+
+  typeEffect();
 });
